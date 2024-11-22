3c179bac8508138f560553ac5fad5166e4eeb85c
hong joon Kim <tkdlqm2@naver.com>
2024-11-22 16:42:29 +0900

    오타 수정

--- app-service/src/main/java/com/service/Application.java
+++ app-service/src/main/java/com/service/Application.java
@@ -1,6 +1,5 @@
 package com.service;
 
-import com.service.config.JacksonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
